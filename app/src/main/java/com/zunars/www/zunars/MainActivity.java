66186fd411b1b6ed964e2eb7b829e892fb905bfe
洽洽 <linqiaqia0622@gmail.com>
2016-04-17 17:14:03 +0800

    改写整个app主页架构，加入侧滑菜单和底部栏

--- app/src/main/java/com/zunars/www/zunars/MainActivity.java
+++ app/src/main/java/com/zunars/www/zunars/MainActivity.java
@@ -1,81 +1,174 @@
 package com.zunars.www.zunars;
 
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-import android.support.v4.app.FragmentTabHost;
-import android.view.LayoutInflater;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.View;
-import android.widget.ImageView;
-import android.widget.TabHost;
-import android.widget.TextView;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import com.ashokvarma.bottomnavigation.BottomNavigationBar;
+import com.ashokvarma.bottomnavigation.BottomNavigationItem;
+import com.zunars.www.adapter.SectionsPagerAdapter;
 import com.zunars.www.fragment.FavoriteFragment;
 import com.zunars.www.fragment.HomeFragment;
 import com.zunars.www.fragment.MessageFragment;
 import com.zunars.www.fragment.ReservationFragment;
 import com.zunars.www.fragment.UserFragment;
 
+import java.util.ArrayList;
+import java.util.List;
 
-public class MainActivity extends FragmentActivity {
-    //定义FragmentTabHost对象
-    private FragmentTabHost mTabHost;
-
-    //定义布局
-    private LayoutInflater layoutInflater;
-
-    //定义数组来存放Fragment界面
-    private Class mFragmentArray[] = {HomeFragment.class, FavoriteFragment.class, ReservationFragment.class, MessageFragment.class, UserFragment.class};
-
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
     //Tab选项卡的文字
     private String mTextviewArray[] = {"首页", "消息", "好友", "广场", "更多"};
-
+    BottomNavigationBar bottomNavigationBar;
+    ViewPager mViewPager;
     //定义数组来存放按钮图片
-    private int mImageViewArray[] = {R.drawable.home,R.drawable.home,R.drawable.home,
-            R.drawable.home,R.drawable.home};
-
-
+    private int mImageViewArray[] = {R.drawable.home, R.drawable.home, R.drawable.home,
+            R.drawable.home, R.drawable.home};
+private SectionsPagerAdapter mSectionsPagerAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        
 
-        initView();
+        mViewPager=(ViewPager)findViewById(R.id.container);
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        List<Fragment> lf = new ArrayList<>();
+        // lf.add( new view1_fragment());
+        lf.add( new HomeFragment());
+        lf.add( new MessageFragment());
+        lf.add( new UserFragment());
+        lf.add( new FavoriteFragment());
+        lf.add( new ReservationFragment());
+
+     
+
+        mSectionsPagerAdapter = new SectionsPagerAdapter(lf, getSupportFragmentManager());
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+        initBottomNavigationBar();
+        
     }
 
-    //***********************************************************
-    //初始化Fragment
-    //***********************************************************
-    private void initView() {
-        layoutInflater = LayoutInflater.from(this);
+    public void initBottomNavigationBar() {
+        bottomNavigationBar = (BottomNavigationBar) findViewById(R.id.bottom_navigation_bar);
+        for (int i = 0; i < 5; i++) {
+            bottomNavigationBar.addItem(new BottomNavigationItem(mImageViewArray[i], mTextviewArray[i]));
+        }
+        bottomNavigationBar.setTabSelectedListener(new BottomNavigationBar.OnTabSelectedListener(){
+            @Override
+            public void onTabSelected(int position) {
+                mViewPager.setCurrentItem(position);
+            }
+            @Override
+            public void onTabUnselected(int position) {
+            }
+            @Override
+            public void onTabReselected(int position) {
+            }
+        });
+        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                
+            }
 
-        //实例化TabHost对象，得到TabHost
-        mTabHost = (FragmentTabHost)findViewById(android.R.id.tabhost);
-        mTabHost.setup(this, getSupportFragmentManager(), R.id.main_content);
+            @Override
+            public void onPageSelected(int position) {
+                bottomNavigationBar.selectTab(position);
+            }
 
-        int count = mFragmentArray.length;
-        for(int i = 0; i < count; i++){
-            //为每一个Tab按钮设置图标、文字和内容
-            TabHost.TabSpec tabSpec = mTabHost.newTabSpec(mTextviewArray[i]).setIndicator(getTabItemView(i));
-            //将Tab按钮添加进Tab选项卡中
-            mTabHost.addTab(tabSpec, mFragmentArray[i], null);
-            //设置Tab按钮的背景
-            mTabHost.getTabWidget().getChildAt(i).setBackgroundResource(R.drawable.selector_tab_background);
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+        bottomNavigationBar
+                .setMode(BottomNavigationBar.MODE_CLASSIC);
+        bottomNavigationBar
+                .setBackgroundStyle(BottomNavigationBar.BACKGROUND_STYLE_RIPPLE);
+        bottomNavigationBar
+                .setActiveColor(R.color.colorGreen)
+                .setInActiveColor("#FFFFFF")
+                .setBarBackgroundColor("#ECECEC"); 
+        
+          bottomNavigationBar.initialise();
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
         }
     }
 
-    /**
-     * 给Tab按钮设置图标和文字
-     */
-    private View getTabItemView(int index){
-        View view = layoutInflater.inflate(R.layout.fragment_main_tab, null);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.home, menu);
+        return true;
+    }
 
-        ImageView imageView = (ImageView) view.findViewById(R.id.imageview);
-        imageView.setImageResource(mImageViewArray[index]);
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
 
-        TextView textView = (TextView) view.findViewById(R.id.textview);
-        textView.setText(mTextviewArray[index]);
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
 
-        return view;
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
